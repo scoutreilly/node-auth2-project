23d0d6bd63a8174d5d3948a2855b20314c6d7ffa
scoutreilly <scouty33@gmail.com>
2022-01-16 23:36:37 -0700

    almost finished

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -6,6 +6,24 @@
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
  */
-module.exports = {
+const JWT = require("jsonwebtoken");
+// process.env is only available on Heroku, devs use "keepitsafe"
+const JWT_SECRET = process.env.JWT_SECRET || "keepitsafe";
 
-}
+const makeToken = (user) => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  };
+  // 20s - seconds you can do y - years d - days and so on
+  const options = {
+    expiresIn: "200s",
+  };
+  return JWT.sign(payload, JWT_SECRET, options);
+};
+
+module.exports = {
+  makeToken,
+  JWT_SECRET,
+};
